46fb8b769011a60d29b3b1aacd283846b7116644
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-12 16:58:11 +0200

    style: apply JuliaFormatter to JET tests

--- test/jet_metric.jl
+++ test/jet_metric.jl
@@ -96,7 +96,9 @@ jet_identity_fixture_other(x::String) = x + 1
     class = "MethodErrorReport"
     reason = "too broad"
     """)
-    @test_throws ErrorException EXT.dismissed(MethodErrorReport(StubFrame[], "anything"), rulings)
+    @test_throws ErrorException EXT.dismissed(
+      MethodErrorReport(StubFrame[], "anything"), rulings
+    )
     @test_throws ErrorException EXT.validate_dismissals(rulings)
   end
 
